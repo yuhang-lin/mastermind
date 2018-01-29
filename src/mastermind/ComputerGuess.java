fd8e976666a0833756a95d81fa522c2d186b061e
Yuhang Lin <ylin19@uvm.edu>
2018-01-29 14:05:47 -0500

    Add ComputerGuess

---
+++ src/mastermind/ComputerGuess.java
@@ -0,0 +1,14 @@
+package mastermind;
+
+public class ComputerGuess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void computerGuess() {
+		System.out.println("Computer will guess what's in your mind.");
+	}
+
+}
