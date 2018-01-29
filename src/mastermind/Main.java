a16e2a4972b6d8d120cbfe39c55c524d2941b7c6
Yuhang Lin <ylin19@uvm.edu>
2018-01-29 14:43:55 -0500

    Format the code

--- src/mastermind/Main.java
+++ src/mastermind/Main.java
@@ -19,14 +19,16 @@ public class Main {
 	public static void main(String[] args) {
 		start();
 	}
-	
+
 	public static void start() {
-		String welcome = "Welcome to Mastermind!\nWould you like to guess what is in computer's mind? \nOr would you like the computer to guess what is in your mind?";
+		String welcome = "Welcome to Mastermind!\nWould you like to guess what is in computer's mind? \n"
+				+ "Or would you like the computer to guess what is in your mind?";
 		System.out.println(welcome);
 		char choice = '0';
 		try (Scanner scanner = new Scanner(System.in)) {
 			while (choice != '1' && choice != '2') {
-				System.out.println("Please make your choice by typing 1 if you want to guess by yourself, or 2 if you want the computer to guess");
+				System.out.println("Please make your choice by typing 1 if you want to guess by yourself, "
+						+ "or 2 if you want the computer to guess");
 				choice = scanner.next().charAt(0);
 				switch (choice) {
 				case '1':
@@ -38,7 +40,7 @@ public class Main {
 				}
 			}
 		}
-		
+
 	}
 
 }
