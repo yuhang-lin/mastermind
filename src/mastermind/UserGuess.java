5b4873c9e7f93208715de6749438ba21ae1b67a2
Yuhang Lin <ylin19@uvm.edu>
2018-01-30 09:55:38 -0500

    Store user guesses as integers

--- src/mastermind/UserGuess.java
+++ src/mastermind/UserGuess.java
@@ -6,8 +6,9 @@ import java.util.Scanner;
 
 public class UserGuess {
 	public static int total_guesses = 0;
-	public static String[] comp_array = new String[4];
+	private static final int NUM_COLOR_ROUND = 4; // Number of colors for each round
 	private static final int MAX_GUESS = 12; // Maximum number of guesses allowable
+	public static int[] comp_array = new int[NUM_COLOR_ROUND];
 
 	public static void main(String[] args) {
 		userGuess();
@@ -18,72 +19,60 @@ public class UserGuess {
 		System.out.println("It's time for you to guess. ");
 		System.out.println("The computer will pick a sequence of 4 colors from the 6 available");
 		System.out.println("You will try to guess what that exact sequence is in 12 tries, good luck!");
+		String[] colors = { "blue", "white", "green", "yellow", "orange", "red" };
+
 		// have computer pick random sequence
-		for (int i = 0; i < 4; i++) {
-			comp_array[i] = gen_rand();
+		Random random = new Random();
+		for (int i = 0; i < NUM_COLOR_ROUND; i++) {
+			comp_array[i] = random.nextInt(colors.length) + 1;
 		}
 		System.out.println("Computer generated array: " + Arrays.toString(comp_array));
-
-		// total number of guesses so far
-		total_guesses++;
-
 		// scanner to input user data
-		Scanner user_input = new Scanner(System.in);
+		try (Scanner scanner = new Scanner(System.in)) {
+			// allows the user to input 12 guesses
+			while (total_guesses < MAX_GUESS) {
+				// total number of guesses so far
+				total_guesses++;
+				System.out.print("Please enter four digits: ");
+				for (int i = 0; i < colors.length; i++) {
+					System.out.print(String.format("%d for %s", i + 1, colors[i]));
+					if (i != colors.length - 1) {
+						System.out.print(", "); // Separate colors with comma
+					} else {
+						System.out.println(); // Print a new line for the last color option
+					}
+				}
+				// reads the line into an array
+				int[] user_guesses = new int[NUM_COLOR_ROUND];
+				int index = 0;
 
-		// queues the user
-		System.out.println("Enter a guess: ");
+				while (index < NUM_COLOR_ROUND) {
+					// reads the entire user input into a line
+					String guess = scanner.nextLine();
+					for (char ch : guess.toCharArray()) {
+						if (ch > '0' && ch < '7') {
+							user_guesses[index++] = ch - '0';
+						}
+					}
+					int digitLeft = NUM_COLOR_ROUND - index;
+					if (digitLeft > 0) {
+						System.out.println(String.format("Please enter %d more digits", digitLeft));
+					}
+				}
+				// prints out total number of guesses so far
+				System.out.println("Total number of guesses: " + total_guesses);
 
-		// reads the entire user input into a line
-		String guess = user_input.nextLine();
-
-		// reads the line into an array----does not work--can add more than 4
-		String[] user_guesses = new String[4];
-
-		// splits the line by spaces and puts in array
-		user_guesses = guess.split(" ");
-
-		// only allows 4 spots and undoes a guess
-		if (user_guesses.length > 4) {
-			System.out.println("Only 4 slots are allowed. 1 Guess undone. ");
-			total_guesses = total_guesses - 1;
+				// checks if user wins
+				if (Arrays.equals(user_guesses, comp_array)) {
+					System.out.println("You win!");
+					break;
+				}
+			}
 		}
-
-		// test prints out user input
-		// System.out.println(guess);
-
-		// prints out actual array
-		System.out.println(Arrays.toString(user_guesses));
-
-		// test to print out each index of array
-		System.out.println(user_guesses[0]);
-		System.out.println(user_guesses[1]);
-		System.out.println(user_guesses[2]);
-		System.out.println(user_guesses[3]);
-
-		// prints out total number of guesses so far
-		System.out.println("Total number of guesses: " + total_guesses);
-
-		// if user has done 12 guesses, game is over
-		if (total_guesses > MAX_GUESS) {
+		if (total_guesses == MAX_GUESS) {
+			// if user has done 12 guesses, game is over
 			System.out.println("You lose!");
+			System.out.println("Here is what computer generated: " + Arrays.toString(comp_array));
 		}
-
-		// checks if user wins
-		if (Arrays.equals(user_guesses, comp_array)) {
-			System.out.println("You win!");
-		}
-
-		// allows the user to input 12 guesses
-		while (total_guesses < MAX_GUESS) {
-			userGuess();
-		}
-
-	}
-
-	private static String gen_rand() {
-		String[] colors = { "blue", "white", "green", "yellow", "orange", "red" };
-		Random random = new Random();
-		String banana = colors[random.nextInt(colors.length)];
-		return banana;
 	}
 }
