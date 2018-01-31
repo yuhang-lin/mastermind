73e0fa31a10b3fc8dd419ff9ecab732caf61df88
Yuhang Lin <ylin19@uvm.edu>
2018-01-30 19:27:28 -0500

    Fix bug of counting the same colors for more than one time & Add main function for testing

--- src/mastermind/Guess.java
+++ src/mastermind/Guess.java
@@ -1,11 +1,19 @@
 package mastermind;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class Guess {
 	public static int total_guesses = 0;
 	protected static final int NUM_COLOR_ROUND = 4; // Number of colors for each round
 	protected static final int MAX_GUESS = 12; // Maximum number of guesses allowable
+	
+	public static void main(String[] args) {
+		int[] correct = {4, 5, 1, 5};
+		int[] guess = {1, 1, 2, 2};
+		int[] result = compareGuess(correct, guess);
+		System.out.println(Arrays.toString(result));
+	}
 	
 	protected static int[] compareGuess (int[] correct, int[] guess) {
 		int[] result = new int[2];
@@ -24,6 +32,7 @@ public class Guess {
 					} else if (correct[i] == guess[j]) {
 						used.add(j);
 						result[1]++;
+						break;
 					}
 				}
 			}
