f26cc08e2771ec88467fa9c2342c433d1e08cb55
Yuhang Lin <ylin19@uvm.edu>
2018-02-07 11:20:26 -0500

    Format the code and fix typo

--- src/mastermind/UserGuess.java
+++ src/mastermind/UserGuess.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  * @author Peter Macksey, Yuhang Lin
  *
  */
-public class UserGuess extends Guess{
+public class UserGuess extends Guess {
 	public static int[] comp_array = new int[NUM_COLOR_ROUND];
 	public static int total_hints = 0;
 
@@ -33,19 +33,17 @@ public class UserGuess extends Guess{
 		for (int i = 0; i < NUM_COLOR_ROUND; i++) {
 			comp_array[i] = random.nextInt(colors.length) + 1;
 		}
-		
-		
-		//TO TEST, DELETE BEFORE FINISHING PROJET
+
+		// TO TEST, DELETE BEFORE FINISHING PROJET
 		System.out.println("TEST TEST TEST Computer generated array: " + Arrays.toString(comp_array));
-		
-		
+
 		// scanner to input user data
 		try (Scanner scanner = new Scanner(System.in)) {
 			// allows the user to input 12 guesses
 			while (total_guesses < MAX_GUESS) {
 				System.out.print("The options are 1-6: (");
-				
-				//prints out all color options
+
+				// prints out all color options
 				for (int i = 0; i < colors.length; i++) {
 					System.out.print(String.format("%d for %s", i + 1, colors[i]));
 					if (i != colors.length - 1) {
@@ -54,30 +52,30 @@ public class UserGuess extends Guess{
 						System.out.println("):"); // Print a new line for the last color option
 					}
 				}
-				
+
 				System.out.println("Enter 4 numbers: ");
-				
+
 				// reads the line into an array
 				int[] user_guesses = new int[NUM_COLOR_ROUND];
 				int index = 0;
 				while (index < NUM_COLOR_ROUND) {
 					// reads the entire user input into a line
 					String guess = scanner.nextLine();
-					
-					//check if user wants hint
-					if(Objects.equals("hint", guess)){
+
+					// check if user wants hint
+					if (Objects.equals("hint", guess)) {
 						hint();
 					}
-					
-					//checks if user wants to quit
-					if(Objects.equals("quit", guess.toLowerCase()) || Objects.equals("exit", guess.toLowerCase())){
+
+					// checks if user wants to quit
+					if (Objects.equals("quit", guess.toLowerCase()) || Objects.equals("exit", guess.toLowerCase())) {
 						System.out.println("You quit, better luck next time! ");
 						System.exit(0);
 					}
-					
+
 					for (char ch : guess.toCharArray()) {
 						if (index == NUM_COLOR_ROUND) {
-							//if more than 4 digits are used, only the first 4 are entered into array
+							// if more than 4 digits are used, only the first 4 are entered into array
 							System.out.println("Warning: only the first four digits are taken as your guess.");
 							break;
 						}
@@ -87,18 +85,19 @@ public class UserGuess extends Guess{
 					}
 					int digitLeft = NUM_COLOR_ROUND - index;
 					if (digitLeft > 0) {
-						//if <4 digits are entered, then the program asks the user to enter the remaining to make 4
+						// if <4 digits are entered, then the program asks the user to enter the
+						// remaining to make 4
 						System.out.println(String.format("Please enter %d more digits (1 to 6 only)", digitLeft));
 					}
 				}
-				
+
 				// total number of guesses so far
 				total_guesses++;
-				
+
 				// prints out total number of guesses so far
 				System.out.println("Total number of guesses: " + total_guesses);
 				System.out.println("Your guess is: " + Arrays.toString(user_guesses));
-				
+
 				// checks if user wins
 				int[] result = compareGuess(comp_array, user_guesses);
 				int numRightPos = result[0];
@@ -119,29 +118,29 @@ public class UserGuess extends Guess{
 			System.out.println("Here is what computer generated: " + Arrays.toString(comp_array));
 		}
 	}
-	
-	public static void hint(){
-		//method to give user hint if they wish
-		//max of 2 hints allowed, can't make it too easy
-		//allows duplicates and things you've already guessed
-		
+
+	public static void hint() {
+		// method to give user hint if they wish
+		// max of 2 hints allowed, can't make it too easy
+		// allows duplicates and things you've already guessed
 
 		// have computer pick random index for hint
 		Random hint_random = new Random();
 		int hint = hint_random.nextInt(comp_array.length);
-		//makes it easier for poeople to read instead of having the first item be at the 0 place
+		// makes it easier for people to read instead of having the first item be at
+		// the 0 place
 		int index = hint + 1;
-		
+
 		System.out.println("Ah, you want a hint?");
-		if(total_hints == 0) {
+		if (total_hints == 0) {
 			System.out.println("The number in the " + index + " place is: " + comp_array[hint]);
 		}
-		
-		if(total_hints == 1){
+
+		if (total_hints == 1) {
 			System.out.println("There is a " + comp_array[hint] + " somewhere");
 		}
-		
-		if(total_hints > 1){
+
+		if (total_hints > 1) {
 			System.out.println("Too bad, no more hints! ");
 		}
 		total_hints++;
