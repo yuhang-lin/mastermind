79ba62c59fdddd38320d475574fafd8f11d37788
Yuhang Lin <ylin19@uvm.edu>
2018-02-08 15:02:35 -0500

    Add comments && Add 3 more test cases

--- src/test/GuessTest.java
+++ src/test/GuessTest.java
@@ -1,3 +1,6 @@
+/**
+ * Unit test for Guess using JUnit 5
+ */
 package test;
 
 import mastermind.Guess;
@@ -6,6 +9,10 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/**
+ * @author Yuhang Lin
+ *
+ */
 class GuessTest {
 	
 	@Test
@@ -89,6 +96,36 @@ class GuessTest {
 	}
 	
 	@Test
+	@DisplayName("1 Black 1 White")
+	void oneBlackOneWhite() {
+		int[] correctResult = {1, 1};
+		int[] correct = {3, 5, 2, 2};
+		int[] guess = {4, 3, 1, 2};
+		int[] result = Guess.compareGuess(correct, guess);
+		assertArrayEquals(correctResult, result, "1 color is matched and 1 color is in the wrong positions");
+	}
+	
+	@Test
+	@DisplayName("1 Black 2 White")
+	void oneBlackTwoWhite() {
+		int[] correctResult = {1, 2};
+		int[] correct = {3, 5, 2, 2};
+		int[] guess = {2, 3, 1, 2};
+		int[] result = Guess.compareGuess(correct, guess);
+		assertArrayEquals(correctResult, result, "1 color is matched and 2 colors are in the wrong positions");
+	}
+	
+	@Test
+	@DisplayName("1 Black 3 White")
+	void oneBlackThreeWhite() {
+		int[] correctResult = {1, 3};
+		int[] correct = {3, 5, 2, 2};
+		int[] guess = {2, 3, 5, 2};
+		int[] result = Guess.compareGuess(correct, guess);
+		assertArrayEquals(correctResult, result, "1 color is matched and 3 colors are in the wrong positions");
+	}
+	
+	@Test
 	@DisplayName("2 Black 2 White")
 	void twoBlackTwoWhite() {
 		int[] correctResult = {2, 2};
@@ -97,5 +134,5 @@ class GuessTest {
 		int[] result = Guess.compareGuess(correct, guess);
 		assertArrayEquals(correctResult, result, "2 colors are matched and 2 colors are in the wrong positions");
 	}
-	
+
 }
