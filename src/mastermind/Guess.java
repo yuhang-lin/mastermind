095c95961aa8d25870a137303cbd2b743e690c3b
Yuhang Lin <ylin19@uvm.edu>
2018-02-08 13:32:58 -0500

    Add better algorithm to determine guess result

--- src/mastermind/Guess.java
+++ src/mastermind/Guess.java
@@ -3,7 +3,7 @@
  */
 package mastermind;
 
-import java.util.HashSet;
+import java.util.HashMap;
 
 /**
  * @author Yuhang Lin
@@ -13,36 +13,44 @@ public class Guess {
 	public static int total_guesses = 0; // Total number of guesses made so far
 	protected static final int NUM_COLOR_ROUND = 4; // Number of colors for each round
 	protected static final int MAX_GUESS = 12; // Maximum number of guesses allowable
-	
+
 	/**
 	 * Compare the guessed colors with the correct colors.
-	 * @param correct an integer array storing the correct colors
-	 * @param guess an integer array storing the guessed colors
-	 * @return a two element integer array. The first element is the number of correct colors and in the right position
-	 * 		the other is the number of correct colors in the wrong position 
+	 * 
+	 * @param correct
+	 *            an integer array storing the correct colors
+	 * @param guess
+	 *            an integer array storing the guessed colors
+	 * @return a two element integer array. The first element is the number of
+	 *         correct colors and in the right position the other is the number of
+	 *         correct colors in the wrong position
 	 */
-	public static int[] compareGuess (int[] correct, int[] guess) {
+	public static int[] compareGuess(int[] correct, int[] guess) {
 		int[] result = new int[2];
-		HashSet<Integer> used = new HashSet<>();
+		// Deal with wrong user inputs
+		if (correct == null || guess == null || correct.length != guess.length || correct.length != NUM_COLOR_ROUND) {
+			return result;
+		}
+		HashMap<Integer, Integer> map = new HashMap<>();
 		for (int i = 0; i < NUM_COLOR_ROUND; i++) {
 			if (correct[i] == guess[i]) {
 				result[0]++; // Increase the number of correct colors and in the right position
-				used.add(i);
 			}
+			map.put(correct[i], 1 + map.getOrDefault(correct[i], 0));
 		}
 		for (int i = 0; i < NUM_COLOR_ROUND; i++) {
-			if (correct[i] != guess[i]) {
-				for (int j = 0; j < NUM_COLOR_ROUND; j++) {
-					if (used.contains(j)) {
-						continue; // We have used this position before
-					} else if (correct[i] == guess[j]) {
-						used.add(j);
-						result[1]++; // Increase the number of correct colors in the wrong position
-						break; // Break the inner loop if we have found such a color
-					}
+			if (map.containsKey(guess[i])) {
+				result[1]++;
+				int count = map.get(guess[i]);
+				if (count == 1) {
+					map.remove(guess[i]);
+				} else {
+					map.put(guess[i], count - 1);
 				}
 			}
 		}
+		// Because each black is added once, we need to subtract them to get the correct value
+		result[1] -= result[0];
 		return result;
 	}
 
