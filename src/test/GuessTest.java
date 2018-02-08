9690d797d3ef6b8a357679a8c240f75845580e26
Yuhang Lin <ylin19@uvm.edu>
2018-02-08 11:42:17 -0500

    Add more test cases

--- src/test/GuessTest.java
+++ src/test/GuessTest.java
@@ -7,6 +7,36 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class GuessTest {
+	
+	@Test
+	@DisplayName("1 Black")
+	void oneBlack() {
+		int[] correctResult = {1, 0};
+		int[] correct = {1, 5, 2, 4};
+		int[] guess = {1, 1, 1, 3};
+		int[] result = Guess.compareGuess(correct, guess);
+		assertArrayEquals(correctResult, result, "1 color is matched");
+	}
+	
+	@Test
+	@DisplayName("2 Black")
+	void twoBlack() {
+		int[] correctResult = {2, 0};
+		int[] correct = {1, 5, 2, 1};
+		int[] guess = {1, 4, 1, 1};
+		int[] result = Guess.compareGuess(correct, guess);
+		assertArrayEquals(correctResult, result, "2 colors are matched");
+	}
+	
+	@Test
+	@DisplayName("3 Black")
+	void threeBlack() {
+		int[] correctResult = {3, 0};
+		int[] correct = {1, 5, 1, 1};
+		int[] guess = {1, 1, 1, 1};
+		int[] result = Guess.compareGuess(correct, guess);
+		assertArrayEquals(correctResult, result, "3 colors are matched");
+	}
 
 	@Test
 	@DisplayName("4 Black")
@@ -16,6 +46,36 @@ class GuessTest {
 		int[] guess = {1, 5, 2, 1};
 		int[] result = Guess.compareGuess(correct, guess);
 		assertArrayEquals(correctResult, result, "All colors should be matached");
+	}
+	
+	@Test
+	@DisplayName("1 White")
+	void oneWhite() {
+		int[] correctResult = {0, 1};
+		int[] correct = {3, 5, 2, 1};
+		int[] guess = {4, 4, 1, 4};
+		int[] result = Guess.compareGuess(correct, guess);
+		assertArrayEquals(correctResult, result, "1 color is in the wrong position");
+	}
+	
+	@Test
+	@DisplayName("2 White")
+	void twoWhite() {
+		int[] correctResult = {0, 2};
+		int[] correct = {3, 5, 3, 1};
+		int[] guess = {2, 3, 4, 3};
+		int[] result = Guess.compareGuess(correct, guess);
+		assertArrayEquals(correctResult, result, "2 colors are in the wrong positions");
+	}
+	
+	@Test
+	@DisplayName("3 White")
+	void threeWhite() {
+		int[] correctResult = {0, 3};
+		int[] correct = {3, 5, 2, 1};
+		int[] guess = {5, 3, 1, 4};
+		int[] result = Guess.compareGuess(correct, guess);
+		assertArrayEquals(correctResult, result, "3 colors are in the wrong positions");
 	}
 	
 	@Test
@@ -37,4 +97,5 @@ class GuessTest {
 		int[] result = Guess.compareGuess(correct, guess);
 		assertArrayEquals(correctResult, result, "2 colors are matched and 2 colors are in the wrong positions");
 	}
+	
 }
