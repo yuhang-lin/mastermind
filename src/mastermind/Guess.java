eab9661bc799b6c0bde24217d0254fb60f7143e2
Yuhang Lin <ylin19@uvm.edu>
2018-02-09 22:46:08 -0500

    Add number suffix function

--- src/mastermind/Guess.java
+++ src/mastermind/Guess.java
@@ -54,4 +54,20 @@ public class Guess {
 		return result;
 	}
 
+	public static String getNumberSuffix(int num) {
+		if (num >= 11 && num <= 13) {
+			return "th";
+		}
+		switch (num % 10) {
+		case 1:
+			return "st";
+		case 2:
+			return "nd";
+		case 3:
+			return "rd";
+		default:
+			return "th";
+		}
+	}
+
 }
