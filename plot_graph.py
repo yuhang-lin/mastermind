06baf12dce04653bfae753d422a9c6bb8a1a5722
Yuhang Lin <ylin19@uvm.edu>
2018-02-14 23:52:09 -0500

    Rename to plot_graph

--- plot_graph.py
+++ plot_graph.py
@@ -11,9 +11,11 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 
+
 file_name = "user_stats.txt"
 num_lost = 0
-win_list = []
+num_won = 0
+guess_list = []
 output_dir = './fig/'
 
 if not os.path.exists(output_dir):
@@ -24,11 +26,13 @@ with open(file_name, "r") as fin:
         num = int(line.strip())
         if num == 0:
             num_lost += 1
+            guess_list.append(12)
         else:
-            win_list.append(num)
+            num_won += 1
+            guess_list.append(num)
 
 # Plot pie chart to compare the percentage of winning and losing
-data = [len(win_list), num_lost]
+data = [num_won, num_lost]
 explode = (0, 0.1)
 labels = ['Winning', 'Losing']
 plt.pie(data, explode=explode, labels=labels, autopct='%1.1f%%',
@@ -41,15 +45,16 @@ plt.show()
 # Plot time series of guesses to win
 plt.ylabel("Number of guesses")
 plt.xlabel("Index of game")
-plt.plot(win_list, marker='o')
+plt.plot(guess_list, marker='o', label='user')
 plt.gcf().set_size_inches(9.0, 6.0)
 plt.savefig(output_dir+'num_guesses_time.png', bbox_inches='tight', dpi=100)
+plt.legend(loc='best')
 plt.show()
 
 # Compare min, max, average number of guesses
-max_guess = max(win_list)
-min_guess = min(win_list)
-mean = np.mean(win_list)
+max_guess = max(guess_list)
+min_guess = min(guess_list)
+mean = np.mean(guess_list)
 width = 0.35
 bar_data = [max_guess, min_guess, mean]
 ind = np.arange(len(bar_data))
